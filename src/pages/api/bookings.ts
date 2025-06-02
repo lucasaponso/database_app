df6a7946b3f035533f101447d6683b00cc2795ab
Lucas Aponso <s3896348@student.rmit.edu.au>
2025-06-02 18:02:23 +1000

    feat: completed register booking feature, also added pop up to notify use that the booking was registered, and they can click a button to take them back to the home page

--- src/pages/api/bookings.ts
+++ src/pages/api/bookings.ts
@@ -1,39 +1,52 @@
 import { MongoClient } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-const uri = process.env.MONGODB_URI;
+const uri: string | undefined = process.env.MONGODB_URI;
 
+const HTTP_200: number = 200
+const HTTP_201: number = 200
+const HTTP_422: number = 422
+const HTTP_500: number = 500
+
+/**
+ * @brief The following endpoint takes data from the client's request
+ * and adds it to the MongoDB collection.
+ * @param req 
+ * @param res 
+ * @returns 
+ */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-  let client;
-  
-  try {
-    client = new MongoClient(uri);
-    await client.connect();
-
-    const db = client.db('sample_airbnb'); // Replace with your database name
-    const collection = db.collection('bookings'); // Replace with your collection name
-
-    // Validate required fields
     const { startDate, endDate, clientName, email, daytimePhone } = req.body;
+    
     if (!startDate || !endDate || !clientName || !email || !daytimePhone) {
-      return res.status(400).json({ message: 'Missing required fields' });
+        return res.status(HTTP_422).json({ message: 'Missing required fields' });
     }
 
-    // Insert the booking
-    const result = await collection.insertOne({
-      ...req.body,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
+    if (uri == undefined) {
+        return res.status(HTTP_500).json({ message: 'API does not have connection to MongoDB'});
+    }
 
-    return res.status(201).json({
-      message: 'Booking created successfully',
-      bookingId: result.insertedId,
-    });
+    let client;
+
+    try {
+        
+        client = new MongoClient(uri);
+        await client.connect();
+
+        const db = client.db('sample_airbnb');
+        const collection = db.collection('bookings');
+
+
+        // Insert the booking
+        const result = await collection.insertOne({...req.body});
+
+        return res.status(HTTP_201).json({
+        message: 'Booking created successfully',
+        bookingId: result.insertedId,
+        });
   } catch (error) {
     console.error('Database error:', error);
-    return res.status(500).json({ message: 'Internal server error' });
+    return res.status(HTTP_500).json({ message: 'Internal server error' });
   } finally {
     if (client) {
       await client.close();
